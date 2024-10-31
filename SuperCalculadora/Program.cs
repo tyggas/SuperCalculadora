02edb7aece5b06c7c2b4e7e85e7df9ee3785bcd7
tyggas <tagrigull@gmail.com>
2024-10-30 22:00:15 -0300

    Programa finalizado

--- SuperCalculadora/Program.cs
+++ SuperCalculadora/Program.cs
@@ -1,57 +1,89 @@
 ﻿using SuperCalculadora;
 
 internal class Program
-{
-    public static void Main(string[] args)
-    {
-        
-        double resultado;
-        string entrada;
 
-        do
+{
+
+    public static void Main(string[] args)
+
+    {
+
+        double resultado;
+
+        Adicao adicao = new Adicao();
+
+        Subtracao subtracao = new Subtracao();
+
+        Divisao divisao = new Divisao();
+
+        Multiplicacao multiplicacao = new Multiplicacao();
+
+        Console.WriteLine("Bem vindos a super calculadora");
+
+        Console.WriteLine("Escolha a operação" +
+
+                          "\nAdição - a" +
+
+                          "\nSubtração - s" +
+
+                          "\nMultiplicação - m" +
+
+                          "\nDivisão - d");
+
+        string entrada = Console.ReadLine();
+
+        Console.WriteLine("Insira o primeiro numero");
+
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Insira o segundo numero");
+
+        int num2 = int.Parse(Console.ReadLine());
+
+        switch (entrada)
+
         {
 
+            case "a":
 
-            Multiplicacao multiplicacao = new Multiplicacao();
-            Console.WriteLine("Bem vindos a super calculadora");
-            Console.WriteLine("Escolha a operação" +
-                              "\nAdição - a" +
-                              "\nSubtração - s" +
-                              "\nMultiplicação - m" +
-                              "\nDivisão - d" +
-                              "\nPara sair do programa - f");
-            entrada = Console.ReadLine();
+                resultado = adicao.Calcular(num1, num2);
 
-            if(entrada == "f")
-            {
+                Console.WriteLine($"O resultado da adição é: {resultado}");
+
                 break;
-            }
-            Console.WriteLine("Insira o primeiro numero");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Insira o segundo numero");
-            int num2 = int.Parse(Console.ReadLine());
 
-            switch (entrada)
-            {
-                case "a":
+            case "s":
 
-                    Console.WriteLine();
-                    break;
-                case "s":
-                    Console.WriteLine();
-                    break;
-                case "m":
-                    resultado = multiplicacao.Calcular(num1, num2);
-                    Console.WriteLine($"O resultado da Multiplicação é: {resultado}");
-                    break;
-                case "d":
-                    Console.WriteLine();
-                    break;
-                default:
-                    Console.WriteLine("Entrada invalida");
-                    break;
-            }
-        }while (entrada != "f");
+                resultado = subtracao.Calcular(num1, num2);
+
+                Console.WriteLine($"O resultado da subtração é: {resultado}");
+
+                break;
+
+            case "m":
+
+                resultado = multiplicacao.Calcular(num1, num2);
+
+                Console.WriteLine($"O resultado da Multiplicação é: {resultado}");
+
+                break;
+
+            case "d":
+
+                resultado = divisao.Calcular(num1, num2);
+
+                Console.WriteLine($"O resultado da divisão é: {resultado}");
+
+                break;
+
+            default:
+
+                Console.WriteLine("Entrada invalida");
+
+                break;
+
+        }
 
     }
+
 }

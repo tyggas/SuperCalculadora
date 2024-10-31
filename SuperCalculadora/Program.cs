b219f6c3551eef79d16b3c5caaec44ee9c2a8827
tyggas <tagrigull@gmail.com>
2024-10-30 21:43:49 -0300

    criação de um loop para a classe program

--- SuperCalculadora/Program.cs
+++ SuperCalculadora/Program.cs
@@ -4,40 +4,54 @@ internal class Program
 {
     public static void Main(string[] args)
     {
+        
         double resultado;
-        Multiplicacao multiplicacao = new Multiplicacao();
-        Console.WriteLine("Bem vindos a super calculadora");
-        Console.WriteLine("Escolha a operação" +
-                          "\nAdição - a" +
-                          "\nSubtração - s" +
-                          "\nMultiplicação - m" +
-                          "\nDivisão - d");
-        string entrada = Console.ReadLine();
-        Console.WriteLine("Insira o primeiro numero");
-        int num1 = int.Parse(Console.ReadLine());
-        Console.WriteLine("Insira o segundo numero");
-        int num2 = int.Parse(Console.ReadLine());
+        string entrada;
 
-        switch (entrada)
+        do
         {
-            case "a":
 
-                Console.WriteLine();
+
+            Multiplicacao multiplicacao = new Multiplicacao();
+            Console.WriteLine("Bem vindos a super calculadora");
+            Console.WriteLine("Escolha a operação" +
+                              "\nAdição - a" +
+                              "\nSubtração - s" +
+                              "\nMultiplicação - m" +
+                              "\nDivisão - d" +
+                              "\nPara sair do programa - f");
+            entrada = Console.ReadLine();
+
+            if(entrada == "f")
+            {
                 break;
-            case "s":
-                Console.WriteLine();
-                break;
-            case "m":
-                resultado = multiplicacao.Calcular(num1,num2);
-                Console.WriteLine($"O resultado da Multiplicação é: {resultado}");
-                break;
-            case "d":
-                Console.WriteLine();
-                break;
-            default:
-                Console.WriteLine("Entrada invalida");
-                break;
-        }
+            }
+            Console.WriteLine("Insira o primeiro numero");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o segundo numero");
+            int num2 = int.Parse(Console.ReadLine());
+
+            switch (entrada)
+            {
+                case "a":
+
+                    Console.WriteLine();
+                    break;
+                case "s":
+                    Console.WriteLine();
+                    break;
+                case "m":
+                    resultado = multiplicacao.Calcular(num1, num2);
+                    Console.WriteLine($"O resultado da Multiplicação é: {resultado}");
+                    break;
+                case "d":
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine("Entrada invalida");
+                    break;
+            }
+        }while (entrada != "f");
 
     }
 }
